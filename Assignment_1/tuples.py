29dd5ea8b469ae6236d127848f2ed608e91b91d2
qwertyioprte <49142993+qwertyioprte@users.noreply.github.com>
2023-01-30 17:03:22 +0530

    Create tuples.py

---
+++ Assignment_1/tuples.py
@@ -0,0 +1,7 @@
+T1=(10,20,30,40)
+T3=("Ani","Rakesh",23,23,"Rajesh")
+print(len(T1))# length of tuple
+print(max(T1))# maximum of tuple
+print(min(T1)) # minimum of tuple
+print(T3.count(23)) # count of element in tuple
+print(T3.index("Ani"))# index of particular element 
