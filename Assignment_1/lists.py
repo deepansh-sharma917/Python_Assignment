6d7945894071c504c577244e7ccc7cf0a60bacab
qwertyioprte <49142993+qwertyioprte@users.noreply.github.com>
2023-01-30 18:02:34 +0530

    Update lists.py

--- Assignment_1/lists.py
+++ Assignment_1/lists.py
@@ -1,1 +1,27 @@
 lst=[1,2,3,4]
+lst2=["Rajesh","Ramesh","Suresh"]
+print(len(lst)) # length of list
+# append method
+lst.append("Hello")
+lst.append(True)
+print("The values after adding in list",lst)
+# extend method
+lst.extend([7,8])
+print(lst)
+# insert method
+lst.insert(3,"Deepansh")
+print(lst)
+# Delete element using remove
+lst.remove(2)
+print(lst)
+#Reverse the list
+lst.reverse()
+print(lst)
+#Delete element using pop
+lst.pop()
+print(lst)
+#index
+print(lst.index("Hello"))
+#slicing
+print(lst[:3])
+print(lst2.index("Ramesh"))
